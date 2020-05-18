bf343de4558ea962c481a52d6eda9a549272e6bb
quocanh31 <phamquocanh31@gmail.com>
2020-05-18 13:21:39 +0700

    fix login

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -8,6 +8,7 @@ package com.example.demo.model;
 import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -45,7 +46,7 @@ public class User {
    @Column(name ="password", nullable = false)
    private String password;
    
-   @ManyToMany
+   @ManyToMany(fetch = FetchType.EAGER)
    @JoinTable(
            name = "user_role",
            joinColumns = @JoinColumn(name = "user_id"),
